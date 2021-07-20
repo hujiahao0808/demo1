320f0a45f8138d154f6f0cb5a8149b643403337b
hujiahao0808 <1712548497@qq.com>
2021-07-20 14:31:28 +0800

    fifth commit

--- pages/dk/dk.js
+++ pages/dk/dk.js
@@ -1,66 +1,29 @@
 // pages/dk/dk.js
 Page({
   data: {
-    array1: [{text:"1",color:"s1"},{text:"2",color:"s1"},{text:"3",color:"s1"},{text:"4",color:"s1"},{text:"5",color:"s1"},{text:"6",color:"s1"},{text:"7",color:"s1"}],
-    array2: [{text:"1",color:"s1"},{text:"2",color:"s1"},{text:"3",color:"s1"},{text:"4",color:"s1"},{text:"5",color:"s1"},{text:"6",color:"s1"},{text:"7",color:"s1"}],
-    array3: [{text:"1",color:"s1"},{text:"2",color:"s1"},{text:"3",color:"s1"},{text:"4",color:"s1"},{text:"5",color:"s1"},{text:"6",color:"s1"},{text:"7",color:"s1"}],
-    array4: [{text:"1",color:"s1"},{text:"2",color:"s1"},{text:"3",color:"s1"},{text:"4",color:"s1"},{text:"5",color:"s1"},{text:"6",color:"s1"},{text:"7",color:"s1"}],
-    array5: [{text:"1",color:"s1"},{text:"2",color:"s1"},{text:"3",color:"s1"},{text:"4",color:"s1"},{text:"5",color:"s1"},{text:"6",color:"s1"},{text:"7",color:"s1"}]
+    daystyle:[],
+    selected:[0,1,2,2,1,1,0,2,2]
   },
-
-  changeColor1:function(e) {
-    var idx=e.currentTarget.dataset.index
-    if (this.data.array1[idx].color==="s1"){
-      this.data.array1[idx].color="s2"
-    } else if (this.data.array1[idx].color==="s2") {
-      this.data.array1[idx].color="s1"
+  onLoad: function (options) {
+    var daystyle = this.data.daystyle
+    var myDate = new Date();
+    var day0 = myDate.getDate();
+    daystyle.push({month: 'current', day: day0, color: 'red', background: '#ffffff'});
+    for(var i = 1; i < day0; i++) {
+      var selected=this.data.selected[i-1]
+      if (selected==2) {
+        daystyle.push({month: 'current', day: i, color: 'white', background: 'green'});
+      } else if (selected==1) {
+        daystyle.push({month: 'current', day: i, color: 'white', background: 'yellow'});
+      } else {
+        daystyle.push({month: 'current', day: i, color: 'white', background: 'pink'});
+      }
     }
     this.setData({
-      array1: this.data.array1
+      daystyle: this.data.daystyle
     })
   },
-  changeColor2:function(e) {
-    var idx=e.currentTarget.dataset.index
-    if (this.data.array2[idx].color==="s1"){
-      this.data.array2[idx].color="s2"
-    } else if (this.data.array2[idx].color==="s2") {
-      this.data.array2[idx].color="s1"
-    }
-    this.setData({
-      array2: this.data.array2
-    })
-  },
-  changeColor3:function(e) {
-    var idx=e.currentTarget.dataset.index
-    if (this.data.array3[idx].color==="s1"){
-      this.data.array3[idx].color="s2"
-    } else if (this.data.array3[idx].color==="s2") {
-      this.data.array3[idx].color="s1"
-    }
-    this.setData({
-      array3: this.data.array3
-    })
-  },
-  changeColor4:function(e) {
-    var idx=e.currentTarget.dataset.index
-    if (this.data.array4[idx].color==="s1"){
-      this.data.array4[idx].color="s2"
-    } else if (this.data.array4[idx].color==="s2") {
-      this.data.array4[idx].color="s1"
-    }
-    this.setData({
-      array4: this.data.array4
-    })
-  },
-  changeColor5:function(e) {
-    var idx=e.currentTarget.dataset.index
-    if (this.data.array5[idx].color==="s1"){
-      this.data.array5[idx].color="s2"
-    } else if (this.data.array5[idx].color==="s2") {
-      this.data.array5[idx].color="s1"
-    }
-    this.setData({
-      array5: this.data.array5
-    })
+  dayClick: function (event) {
+    console.log(event.detail);
   }
 })
